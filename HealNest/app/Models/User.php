ab2409a9dc6ff3fb31c4eb7564a60ff24ebd8667
Adit Raj <aditraj1102@gmail.com>
2026-05-20 01:47:47 +0530

    mongofixed
    "Fix MongoDB namespace imports"

--- HealNest/app/Models/User.php
+++ HealNest/app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Jenssegers\Mongodb\Auth\User as Authenticatable;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
