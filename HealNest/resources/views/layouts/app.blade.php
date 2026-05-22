2e7fe1d54cd10e9cfed10e8fcb7cd191c63c9f39
Adit Raj <aditraj1102@gmail.com>
2026-05-22 12:22:28 +0530

    "icons"

--- HealNest/resources/views/layouts/app.blade.php
+++ HealNest/resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'HealNest')</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
